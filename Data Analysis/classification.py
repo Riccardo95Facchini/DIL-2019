2549b41f66b86b50ab53687e1aa7be2d5fb57124
Luca <luca1.costa@mail.polimi.it>
2019-06-09 20:33:40 +0200

    Added Data Analysis Section and Code

---
+++ Data Analysis/classification.py
@@ -0,0 +1,252 @@
+from google.colab import drive 
+drive.mount('/content/gdrive')
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report
+
+#THE DATASET IS RETRIEVED FROM DRIVE, IT CAN ALSO BE DOWNLOADED FROM UCI AND LOADED LOCALLY
+
+input_file = "/content/gdrive/My Drive/Colab Notebooks/bank-full.csv"
+dataset = pd.read_csv(input_file, sep=';', header = 0)
+
+dataset.head()
+
+#DELETE NEXT CALLS DATA
+
+dataset = dataset.drop("contact", axis=1)
+dataset = dataset.drop("day", axis=1)
+dataset = dataset.drop("month", axis=1)
+dataset = dataset.drop("duration", axis=1)
+dataset = dataset.drop("campaign", axis=1)
+dataset = dataset.drop("pdays", axis=1)
+dataset = dataset.drop("previous", axis=1)
+dataset = dataset.drop("poutcome", axis=1)
+
+dataset.head()
+
+#FEATURE ENGINEERING
+
+cleanup_nums = {"marital":     {"married": 1, "single": 0, "divorced":-1},
+                "education": {"primary": 1, "secondary": 2, "tertiary": 3},
+               "default":     {"yes": 1, "no": 0},
+               "housing":     {"yes": 1, "no": 0},
+               "loan":     {"yes": 1, "no": 0},
+               "y":     {"yes": 1, "no": 0}}
+
+dataset.replace(cleanup_nums, inplace=True)
+dataset.head()
+
+dataset.dtypes
+
+dataset = dataset[dataset.job != 'unknown']
+dataset = dataset[dataset.education != 'unknown']
+dataset['education'] = dataset['education'].astype(int)
+
+#COLLERATION MATRIX
+
+plt.figure(figsize=(12,10))
+cor = dataset.corr()
+sns.heatmap(cor, annot=True, cmap=plt.cm.Reds)
+plt.show()
+
+#CLASSIFIFICATION
+
+X = dataset.iloc[:, 0:7]
+y = dataset.iloc[:, 7]
+
+X = pd.get_dummies(X, columns=["job"], prefix=["job"])
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+#DECISION TREE
+
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+
+clf_dt = DecisionTreeClassifier()
+clt_dt = clf_dt.fit(X_train,y_train)
+
+esito = clf_dt.predict(X_test)
+
+target_names = ['NOT-sub', 'Subscribed']
+print(classification_report(y_test, esito,target_names=target_names))
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, esito)
+print(cm)
+
+plt.hist(esito)
+
+#RANDOM FOREST
+
+from sklearn.ensemble import RandomForestClassifier
+
+clf_dt = RandomForestClassifier()
+clt_dt = clf_dt.fit(X_train,y_train)
+
+esito = clf_dt.predict(X_test)
+
+target_names = ['NOT-sub', 'Subscribed']
+print(classification_report(y_test, esito,target_names=target_names))
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, esito)
+print(cm)
+
+plt.hist(esito)
+
+# K-NEAREST NEIGHBOURS
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# TRAINING - TEST
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# SCALING
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# FITTING
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+classifier.fit(X_train, y_train)
+
+# PREDICTION
+y_pred = classifier.predict(X_test)
+
+# CONFUSION MATRIX
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+target_names = ['NOT-sub', 'Subscribed']
+print(classification_report(y_test, y_pred,target_names=target_names))
+
+print(cm)
+
+plt.hist(y_pred)
+
+#UNDERSAMPLING
+
+from sklearn.utils import resample
+
+dataset_sample = pd.get_dummies(dataset, columns=["job"], prefix=["job"])
+
+#SPLIT FEATURE AND TARGET
+y = dataset_sample.y
+X = dataset_sample.drop('y', axis=1)
+
+#TRAIN TEST
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+X = pd.concat([X_train, y_train], axis=1)
+
+#SELECTING TARGET CLASSES
+
+not_sub = X[X.y==0]
+sub = X[X.y==1]
+
+not_sub_downsampled = resample(not_sub,
+                                replace = False,
+                                n_samples = len(sub),
+                                random_state = 27)
+
+# COMBINE MINORITY AND DOWNSAMPLED MAJORITY
+downsampled = pd.concat([not_sub_downsampled, sub])
+
+#DECISION TREE
+
+y_train = downsampled.y
+X_train = downsampled.drop('y', axis=1)
+
+clf_dt = DecisionTreeClassifier()
+clt_dt = clf_dt.fit(X_train,y_train)
+
+esito = clf_dt.predict(X_test)
+
+target_names = ['NOT-sub', 'Subscribed']
+print(classification_report(y_test, esito,target_names=target_names))
+
+#RANDOM FOREST
+y_train = downsampled.y
+X_train = downsampled.drop('y', axis=1)
+
+clf_dt = RandomForestClassifier()
+clt_dt = clf_dt.fit(X_train,y_train)
+
+esito = clf_dt.predict(X_test)
+
+target_names = ['NOT-sub', 'Subscribed']
+print(classification_report(y_test, esito,target_names=target_names))
+
+#SMOTE - DECISION TREE
+
+from imblearn.over_sampling import SMOTE
+
+#SPLIT FEATURE TARGET
+y = dataset_sample.y
+X = dataset_sample.drop('y', axis=1)
+
+#TRAIN TEST
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+#SMOTE
+sm = SMOTE(random_state=27, ratio=1.0)
+X_train, y_train = sm.fit_sample(X_train, y_train)
+
+clf_dt = DecisionTreeClassifier()
+
+#FIT
+smote = clf_dt.fit(X_train,y_train)
+
+#PREDICITON
+smote_pred = smote.predict(X_test)
+
+target_names = ['NOT-sub', 'Subscribed']
+print(classification_report(y_test, smote_pred,target_names=target_names))
+
+#SMOTE - RANDOM FOREST
+
+from imblearn.over_sampling import SMOTE
+
+y = dataset_sample.y
+X = dataset_sample.drop('y', axis=1)
+
+# setting up testing and training sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+sm = SMOTE(random_state=27, ratio=1.0)
+X_train, y_train = sm.fit_sample(X_train, y_train)
+
+clf_dt = RandomForestClassifier()
+
+smote = clf_dt.fit(X_train,y_train)
+
+smote_pred = smote.predict(X_test)
+
+target_names = ['NOT-sub', 'Subscribed']
+print(classification_report(y_test, smote_pred,target_names=target_names))
+
+#RECAP on RECALL
+
+x = np.arange(3)
+plt.bar(x-0.2, [31,65,37], width=0.2, color='b', align='center', label='DT')
+plt.bar(x, [18,61,32], width=0.2, color='r', align='center', label='RF')
+plt.xticks(x-0.1, ['Normal','Under','Smote'])
+plt.legend(loc='upper right')
+
+#RECAP on F1
+
+x = np.arange(3)
+plt.bar(x-0.2, [31,26,32], width=0.2, color='b', align='center', label='DT')
+plt.bar(x, [24,28,31], width=0.2, color='r', align='center', label='RF')
+plt.xticks(x-0.1, ['Normal','Under','Smote'])
+plt.legend(loc='lower right')
---
+++ Data Analysis/clustering.py
@@ -0,0 +1,126 @@
+#DRIVE CONNECTION
+
+from google.colab import drive 
+drive.mount('/content/gdrive')
+
+#CLUSTERING
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas import read_csv
+from pandas import DataFrame
+from pandas import concat
+from pandas import datetime
+from numpy import concatenate
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+
+#THE DATASET IS RETRIEVED FROM DRIVE, IT CAN ALSO BE DOWNLOADED FROM UCI AND LOADED LOCALLY
+
+input_file = "/content/gdrive/My Drive/Colab Notebooks/total_reviews.csv"
+df = pd.read_csv(input_file, sep=';', header = 0)
+
+df = df[['User Id', 'Religious', 'Sports','Theatre', 'Picnic', 'Nature','Shopping']]
+df.head()
+
+# (Nature - Picnic) Analysis 2 Clusters
+
+X_s = df.iloc[:, [4,5]].values
+
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    kmeans.fit(X_s)
+    wcss.append(kmeans.inertia_)
+
+plt.clf()
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+display(plt.show())
+
+# Fitting K-Means to the dataset
+kmeans = KMeans(n_clusters = 2, init = 'k-means++', random_state = 42)
+y_kmeans = kmeans.fit_predict(X_s)
+
+# Visualising the clusters
+plt.clf()
+plt.scatter(X_s[y_kmeans == 0, 0], X_s[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(X_s[y_kmeans == 1, 0], X_s[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+#plt.scatter(X_s[y_kmeans == 2, 0], X_s[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
+plt.legend()
+display(plt.show())
+
+#Proof
+
+X = df.iloc[:, [4,5]].values
+
+# Using the dendrogram to find the optimal number of clusters
+plt.clf()
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+plt.title('Dendrogram')
+plt.xlabel('Users')
+plt.ylabel('Euclidean distances')
+display(plt.show())
+
+# (Nature - Shopping) Analysis 3 Clusters
+
+X_s = df.iloc[:, [5,6]].values
+
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+    kmeans.fit(X_s)
+    wcss.append(kmeans.inertia_)
+
+plt.clf()
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+display(plt.show())
+
+# Fitting K-Means to the dataset
+kmeans = KMeans(n_clusters = 3, init = 'k-means++', random_state = 42)
+y_kmeans = kmeans.fit_predict(X_s)
+
+# Visualising the clusters
+plt.clf()
+plt.scatter(X_s[y_kmeans == 0, 0], X_s[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(X_s[y_kmeans == 1, 0], X_s[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(X_s[y_kmeans == 2, 0], X_s[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
+plt.legend()
+display(plt.show())
+
+#Proof
+
+X = df.iloc[:, [5,6]].values
+
+# Using the dendrogram to find the optimal number of clusters
+plt.clf()
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+plt.title('Dendrogram')
+plt.xlabel('Users')
+plt.ylabel('Euclidean distances')
+display(plt.show())
+
+#Using Pearson Correlation
+
+import seaborn as sns
+
+input_file = "/content/gdrive/My Drive/Colab Notebooks/total_reviews.csv"
+df = pd.read_csv(input_file, sep=';', header = 0) 
+
+plt.figure(figsize=(12,10))
+cor = df.corr()
+sns.heatmap(cor, annot=True, cmap=plt.cm.Reds)
+plt.show()
